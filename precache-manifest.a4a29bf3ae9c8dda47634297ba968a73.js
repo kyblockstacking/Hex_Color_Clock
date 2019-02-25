df162d7656d2db66fca9864e270bbe4a1865720a
knyg <kevin1992yang@gmail.com>
2019-02-24 21:55:06 -0800

    Updates

---
+++ precache-manifest.a4a29bf3ae9c8dda47634297ba968a73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ef8abe7a3824f2f7e8e",
+    "url": "/kyblockstacking/Hex_Color_Clock/static/css/main.3e9d31b0.chunk.css"
+  },
+  {
+    "revision": "0ef8abe7a3824f2f7e8e",
+    "url": "/kyblockstacking/Hex_Color_Clock/static/js/main.0ef8abe7.chunk.js"
+  },
+  {
+    "revision": "b56dc4b2cc61cff503c6",
+    "url": "/kyblockstacking/Hex_Color_Clock/static/js/1.b56dc4b2.chunk.js"
+  },
+  {
+    "revision": "c0e50ba81c5cba649c68",
+    "url": "/kyblockstacking/Hex_Color_Clock/static/js/runtime~main.c0e50ba8.js"
+  },
+  {
+    "revision": "bcbcbab6374d662223d3b2567208971e",
+    "url": "/kyblockstacking/Hex_Color_Clock/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/Hex_Color_Clock/precache-manifest.1896d38c124be6b3bf852ea598344606.js"
+  "/kyblockstacking/Hex_Color_Clock/precache-manifest.a4a29bf3ae9c8dda47634297ba968a73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Hex_Color_Clock/index.html", {
+workbox.routing.registerNavigationRoute("/kyblockstacking/Hex_Color_Clock/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
